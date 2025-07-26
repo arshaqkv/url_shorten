26cf639a887f0390d91fcf9b494f71a6ec11f557
Muhammed Arshaq <arshaq31@gmail.com>
2025-07-26 14:11:00 +0530

    fix: validation issue & user object return issue

--- backend/src/application/use-cases/Signup.ts
+++ backend/src/application/use-cases/Signup.ts
@@ -3,21 +3,26 @@ import { IUserRepository } from "../../domain/interface/user.repository";
 import { CustomError } from "../../interface/middlewares/error.middleware";
 import bcryptjs from "bcryptjs";
 import { SignUpDTO } from "../dto/user.dto";
+import { HttpStatus } from "../../utils/http.status";
 
 export class Signup {
   constructor(private userRepository: IUserRepository) {}
 
-  async execute(data: SignUpDTO): Promise<User> {
+  async execute(data: SignUpDTO): Promise<void> {
     const { name, email, password } = data;
+
+    if (!name || !email || !password) {
+      throw new CustomError("All fields required", HttpStatus.BAD_REQUEST);
+    }
     const existingUser = await this.userRepository.findByEmail(email);
 
     if (existingUser) {
-      throw new CustomError("Email already in use", 400);
+      throw new CustomError("Email already in use", HttpStatus.BAD_REQUEST);
     }
 
     const hashedPassword = await bcryptjs.hash(password, 10);
     const newUser = new User(name, email, hashedPassword);
 
-    return await this.userRepository.createUser(newUser);
+    await this.userRepository.createUser(newUser);
   }
 }
