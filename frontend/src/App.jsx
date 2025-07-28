91cd859ce6e98a8a997ff98b5ea2343c6bde91bb
Muhammed Arshaq <arshaq31@gmail.com>
2025-07-28 23:48:34 +0530

    add: routes for pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,55 @@
-import React from 'react'
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AppLayout from "./layouts/AppLayout";
+import LandingPage from "./pages/LandingPage";
+import AuthPage from "./pages/AuthPage";
+import AnlyticsPage from "./pages/AnlyticsPage";
+import { ThemeProvider } from "./context/ThemeContext";
+import { Toaster } from "react-hot-toast";
+import ProtectedRoute from "./components/ProtectedRoute";
+import LinkPage from "./pages/LinkPage";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <LandingPage />,
+      },
+      {
+        path: "/auth",
+        element: <AuthPage />,
+      },
+      {
+        path: "/link",
+        element: (
+          <ProtectedRoute>
+            <LinkPage />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/link/:id/analytics",
+        element: (
+          <ProtectedRoute>
+            <AnlyticsPage />
+          </ProtectedRoute>
+        ),
+      },
+    ],
+  },
+]);
 
 const App = () => {
   return (
-    <div>
-      <h1 className='text-amber-300'>Hello world</h1>
-    </div>
-  )
-}
+    <>
+      <Toaster />
+      <ThemeProvider>
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </>
+  );
+};
 
-export default App
+export default App;
---
+++ frontend/src/layouts/AppLayout.jsx
@@ -0,0 +1,16 @@
+import Header from "../components/Header";
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+const AppLayout = () => {
+  return (
+    <div>
+      <main className="min-h-screen container">
+        <Header />
+        <Outlet />
+      </main>
+    </div>
+  );
+};
+
+export default AppLayout;
