3b93b060ae8f65339016c180e2bb9df3a94199e0
Muhammed Arshaq <arshaq31@gmail.com>
2025-07-28 17:38:42 +0530

    add: shadcn components for better ui

--- frontend/src/components/DarkModeToggler.jsx
+++ frontend/src/components/DarkModeToggler.jsx
@@ -5,7 +5,10 @@ import React from "react";
 const DarkModeToggler = () => {
   const { darkMode, setDarkMode } = useDarkMode();
   return (
-    <button onClick={() => setDarkMode(!darkMode)}>
+    <button
+      onClick={() => setDarkMode(!darkMode)}
+      className="bg-gray-200 dark:bg-gray-800 rounded-md px-4 cursor-pointer hover:text-blue-300 dark:hover:text-yellow-600"
+    >
       {darkMode ? <SunMedium size={18} /> : <Moon size={18} />}
     </button>
   );
